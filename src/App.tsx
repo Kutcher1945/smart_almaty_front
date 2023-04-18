e024458ebb563510067641709eae77702b395f20
Adilan Akhramovich <adilan.akhramovich@gmail.com>
2023-04-18 10:20:20 +0600

    updated images

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from './Header';
 import Footer from './Footer';
 import ImagePage from './ImagePage';
 import ButtonGrid from './ButtonsGrid';
-import backgroundImage from './header.jpg';
+import backgroundImage from './mountain.png';
 import './reset.css'
 
 function App() {
@@ -14,7 +14,8 @@ function App() {
       <Header />
       <ImagePage />
       <ButtonGrid buttons={['Ситуационный центр', 'Мониторинг районов', 'Экономика города', 'Социальная жизнь', 'Стартап экосистема', 'Экология города']} />
-      <div style={{backgroundColor:'white'}}></div>
+      {/* <div style={{backgroundColor:'red'}}></div> */}
+      <img src={backgroundImage} alt="Background" className="background-image" />
       <Footer />
     </div>
   );
